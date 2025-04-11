ada4ba1f4b1b37ed9414f42b042dd5004eebdef4
lalah7 <114954233+lalah7@users.noreply.github.com>
2025-04-11 13:46:24 -0400

    updates

--- database.js
+++ database.js
@@ -1,17 +1,44 @@
 const locationDatabase = [
     {
+        id: marker1,
         coordinates: [43.62,-79.39],
         title: "Sewage Issue",
         draggable: false
     },
     {
+        id: marker2,
         coordinates: [43.7, -79.27],
         title: "Graffiti",
         draggable: false
     },
     {
-        coordinates: [43.61,-79.31],
+        id: marker3,
+        coordinates: [43.70,-79.38],
         title: "Water Leak",
+        draggable: false 
+    },
+    {
+        id: marker4,
+        coordinates: [43.73,-79.388],
+        title: "Sewage Issue",
+        draggable: false 
+    },
+    {
+        id: resolved1,
+        coordinates: [43.654,-79.388],
+        title: "Waste Collection",
+        draggable: false 
+    },
+    {
+        id: resolved2,
+        coordinates: [43.675,-79.377],
+        title: "Pest Issue",
+        draggable: false 
+    },
+    {
+        id: resolved3,
+        coordinates: [43.666,-79.443],
+        title: "Graffiti",
         draggable: false 
     }
 ];
